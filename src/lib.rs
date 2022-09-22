eb37dbf577ba340b308fb18e2fd1b0e98e7ac86c
Mendelt Siebenga <mendelt@mendelt.nl>
2022-09-22 22:07:31 +0200

    define expression type

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,45 @@
+use std::collections::hash_map::HashMap;
+
+pub enum Expr {
+    Value(Value),
+    Reference(String),
+    Unary(UnaryOperator),
+    Binary(BinaryOperator),
+}
+
+pub enum Value {
+    String(String),
+    Integer(i128),
+    Float(f64),
+    Bool(bool),
+    Map(HashMap<String, Value>),
+    Vec(Vec<Value>),
+}
+
+pub enum UnaryOperator {
+    Not,
+    Sqrt,
+}
+
+pub enum BinaryOperator {
+    Add,
+    Sub,
+    Mult,
+    Div,
+    Equals,
+    GreaterThan,
+    LessThan,
+    GreaterThanEquals,
+    LessThanEquals,
+    Index,
+}
+
+impl Expr {
+    pub fn evaluate(&self) -> Value {
+        todo!()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
