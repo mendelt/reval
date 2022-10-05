7a4dc504edc9d9a36a46d0add7eeb3eceef24e51
Mendelt Siebenga <mendelt@mendelt.nl>
2022-10-05 22:12:08 +0200

    some number parsing

--- src/parse.rs
+++ src/parse.rs
@@ -1,9 +1,17 @@
 use nom::{
-    branch::alt, bytes::complete::is_not, character::complete::char, combinator::map,
-    sequence::delimited, IResult,
+    branch::alt,
+    bytes::complete::is_not,
+    character::complete::{char, digit1},
+    combinator::{map, map_res, recognize},
+    number::complete::double,
+    sequence::delimited,
+    IResult,
 };
 
-use crate::{expr::Expr, value::Value};
+use crate::{
+    expr::Expr,
+    value::{Number, Value},
+};
 
 pub fn parse(input: &str) -> Expr {
     let (remaining, expr) = expr(input).unwrap();
@@ -16,36 +24,49 @@ fn expr(input: &str) -> IResult<&str, Expr> {
 }
 
 fn value(input: &str) -> IResult<&str, Value> {
-    string_value(input)
+    alt((number_value, string_value))(input)
+}
+
+fn number_value(input: &str) -> IResult<&str, Value> {
+    map(
+        alt((
+            map(map_res(recognize(digit1), str::parse), Number::Int),
+            map(double, Number::Float),
+        )),
+        Value::Number,
+    )(input)
 }
 
 fn string_value(input: &str) -> IResult<&str, Value> {
-    map(alt((double_quoted_string, single_quoted_string)), |value| {
-        Value::String(value.to_string())
-    })(input)
-}
-
-fn double_quoted_string(input: &str) -> IResult<&str, &str> {
-    delimited(char('"'), is_not("\""), char('"'))(input)
-}
-
-fn single_quoted_string(input: &str) -> IResult<&str, &str> {
-    delimited(char('\''), is_not("'"), char('\''))(input)
+    map(
+        alt((
+            delimited(char('"'), is_not("\""), char('"')),
+            delimited(char('\''), is_not("'"), char('\'')),
+        )),
+        |value: &str| Value::String(value.to_string()),
+    )(input)
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    // #[test]
-    // fn should_parse_integer() {
-    //     assert_eq!(parse("0"), Expr::Value(Value::Number(Number::Int(0))));
-    // }
+    #[test]
+    fn should_parse_integer() {
+        assert_eq!(parse("15"), Expr::Value(Value::Number(Number::Int(15))));
+    }
 
-    // #[test]
-    // fn should_parse_integers() {
-    //     assert_eq!(parse("6"), Expr::Value(Value::Number(Number::Int(6))))
-    // }
+    #[test]
+    #[ignore]
+    fn should_parse_negative_integer() {
+        assert_eq!(parse("-6"), Expr::Value(Value::Number(Number::Int(-6))))
+    }
+
+    #[test]
+    #[ignore]
+    fn should_parse_float() {
+        assert_eq!(parse("38e-1"), Expr::Value(Value::Number(Number::Float(3.8))))
+    }
 
     #[test]
     fn should_parse_double_quoted_string() {
@@ -78,5 +99,4 @@ mod test {
             Expr::Value(Value::String("string \"value\"".to_string()))
         );
     }
-
 }
