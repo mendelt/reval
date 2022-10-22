51b6641c254bac4b1f35c702d6dcaece5e7e7d0b
Mendelt Siebenga <mendelt@mendelt.nl>
2022-10-22 20:52:26 +0200

    reorder expression parser, parse parentheses

--- src/parse/expr.rs
+++ src/parse/expr.rs
@@ -2,99 +2,17 @@ use crate::expr::Expr;
 use crate::parse::value::value;
 use nom::{
     branch::alt, bytes::complete::tag, character::complete::multispace0, combinator::map,
-    error::ParseError, IResult, Parser,
+    error::ParseError, sequence::delimited, IResult, Parser,
 };
 
 pub fn expr(input: &str) -> IResult<&str, Expr> {
     sub_expr(input)
 }
 
-fn sub_expr(input: &str) -> IResult<&str, Expr> {
-    alt((
-        map(
-            binary_expr(add_expr, tag("-"), add_expr),
-            |(left, right)| Expr::Sub(Box::new(left), Box::new(right)),
-        ),
-        add_expr,
-    ))(input)
-}
-
-fn add_expr(input: &str) -> IResult<&str, Expr> {
-    alt((
-        map(
-            binary_expr(div_expr, tag("+"), add_expr),
-            |(left, right)| Expr::Add(Box::new(left), Box::new(right)),
-        ),
-        div_expr,
-    ))(input)
-}
-
-fn div_expr(input: &str) -> IResult<&str, Expr> {
-    alt((
-        map(
-            binary_expr(mult_expr, tag("/"), div_expr),
-            |(left, right)| Expr::Div(Box::new(left), Box::new(right)),
-        ),
-        mult_expr,
-    ))(input)
-}
-
-fn mult_expr(input: &str) -> IResult<&str, Expr> {
-    alt((
-        map(
-            binary_expr(value_expr, tag("*"), mult_expr),
-            |(left, right)| Expr::Mult(Box::new(left), Box::new(right)),
-        ),
-        value_expr,
-    ))(input)
-}
-
-fn value_expr(input: &str) -> IResult<&str, Expr> {
-    map(value, Expr::Value)(input)
-}
-
-pub fn binary_expr<'a, O1, O2, O3, E: ParseError<&'a str>, F, G, H>(
-    mut left: F,
-    mut sep: G,
-    mut right: H,
-) -> impl FnMut(&'a str) -> IResult<&'a str, (O1, O3), E>
-where
-    F: Parser<&'a str, O1, E>,
-    G: Parser<&'a str, O2, E>,
-    H: Parser<&'a str, O3, E>,
-{
-    move |input: &'a str| {
-        let (input, _) = multispace0(input)?;
-        let (input, o1) = left.parse(input)?;
-        let (input, _) = multispace0(input)?;
-        let (input, _) = sep.parse(input)?;
-        let (input, _) = multispace0(input)?;
-        let (input, o2) = right.parse(input)?;
-        let (input, _) = multispace0(input)?;
-
-        Ok((input, (o1, o2)))
-        // right.parse(input).map(|(i, o2)| (i, (o1, o2)))
-    }
-}
-
 #[cfg(test)]
-mod when_parsing_expression {
+mod when_parsing_expressions {
     use super::*;
     use crate::value::Value;
-
-    #[test]
-    fn should_parse_mult() {
-        let (rest, parsed) = expr("1*4").unwrap();
-
-        assert!(rest.is_empty());
-        assert_eq!(
-            parsed,
-            Expr::Mult(
-                Box::new(Expr::Value(Value::Int(1))),
-                Box::new(Expr::Value(Value::Int(4))),
-            )
-        );
-    }
 
     #[test]
     fn should_parse_div() {
@@ -161,6 +79,132 @@ mod when_parsing_expression {
                 Box::new(Expr::Value(Value::Int(1)))
             )
         );
+        assert_eq!(rest, "");
+    }
+}
+
+fn sub_expr(input: &str) -> IResult<&str, Expr> {
+    alt((
+        map(
+            binary_expr(add_expr, tag("-"), add_expr),
+            |(left, right)| Expr::Sub(Box::new(left), Box::new(right)),
+        ),
+        add_expr,
+    ))(input)
+}
+
+fn add_expr(input: &str) -> IResult<&str, Expr> {
+    alt((
+        map(
+            binary_expr(div_expr, tag("+"), add_expr),
+            |(left, right)| Expr::Add(Box::new(left), Box::new(right)),
+        ),
+        div_expr,
+    ))(input)
+}
+
+fn div_expr(input: &str) -> IResult<&str, Expr> {
+    alt((
+        map(
+            binary_expr(mult_expr, tag("/"), div_expr),
+            |(left, right)| Expr::Div(Box::new(left), Box::new(right)),
+        ),
+        mult_expr,
+    ))(input)
+}
+
+fn mult_expr(input: &str) -> IResult<&str, Expr> {
+    alt((
+        map(
+            binary_expr(value_expr, tag("*"), mult_expr),
+            |(left, right)| Expr::Mult(Box::new(left), Box::new(right)),
+        ),
+        parenthesis_expr,
+    ))(input)
+}
+
+#[cfg(test)]
+mod when_parsing_mult {
+    use super::{test_util::should_parse, *};
+    use crate::value::Value;
+
+    #[test]
+    fn should_parse_expr() {
+        should_parse(
+            mult_expr("1*4"),
+            Expr::Mult(
+                Box::new(Expr::Value(Value::Int(1))),
+                Box::new(Expr::Value(Value::Int(4))),
+            ),
+        );
+    }
+
+    #[test]
+    fn should_failover() {
+        should_parse(mult_expr("1"), Expr::Value(Value::Int(1)));
+    }
+}
+
+fn parenthesis_expr(input: &str) -> IResult<&str, Expr> {
+    alt((delimited(tag("("), expr, tag(")")), value_expr))(input)
+}
+
+#[cfg(test)]
+mod when_parsing_parentheses {
+    use crate::value::Value;
+
+    use super::*;
+
+    #[test]
+    fn should_parse_expression_inside() {
+        let (rest, parsed) = parenthesis_expr("(1+1)").unwrap();
+
+        assert_eq!(
+            parsed,
+            Expr::Add(
+                Box::new(Expr::Value(Value::Int(1))),
+                Box::new(Expr::Value(Value::Int(1)))
+            )
+        );
+        assert_eq!(rest, "");
+    }
+}
+
+fn value_expr(input: &str) -> IResult<&str, Expr> {
+    map(value, Expr::Value)(input)
+}
+
+pub fn binary_expr<'a, O1, O2, O3, E: ParseError<&'a str>, F, G, H>(
+    mut left: F,
+    mut sep: G,
+    mut right: H,
+) -> impl FnMut(&'a str) -> IResult<&'a str, (O1, O3), E>
+where
+    F: Parser<&'a str, O1, E>,
+    G: Parser<&'a str, O2, E>,
+    H: Parser<&'a str, O3, E>,
+{
+    move |input: &'a str| {
+        let (input, _) = multispace0(input)?;
+        let (input, o1) = left.parse(input)?;
+        let (input, _) = multispace0(input)?;
+        let (input, _) = sep.parse(input)?;
+        let (input, _) = multispace0(input)?;
+        let (input, o2) = right.parse(input)?;
+        let (input, _) = multispace0(input)?;
+
+        Ok((input, (o1, o2)))
+    }
+}
+
+#[cfg(test)]
+mod test_util {
+    use crate::expr::Expr;
+    use nom::IResult;
+
+    pub fn should_parse(result: IResult<&str, Expr>, expected_expr: Expr) {
+        let (rest, parsed) = result.unwrap();
+        assert_eq!(parsed, expected_expr);
         assert_eq!(rest, "");
     }
 }
