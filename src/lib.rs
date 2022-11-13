5932ff4270648c6d519c2e1b45ce289d62dfc1b8
Mendelt Siebenga <mendelt@mendelt.nl>
2022-11-13 20:49:13 +0100

    better readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+//! Reval, short for Rust Evaluator is a light-weight expression evaluator library. It can be used as a rules-engine or in other situations where simple user expressions need to be evaluated.
+//!
+//! Expressions or rules can be written in a simple DSL or in a json format that is directly parsed into Reval expression AST objects. For now the Reval DSL parser is experimental but eventually this should replace json rules.
+//!
+//! Expressions can be evaluated against input data to produce output data. The input and output data is passed in and out of the rules as a `reval::Value` object which can contain simple data or more complex structures like maps or vectors.
+//! Data can even be nested so complex data can be passed into Reval expressions.
+//!
+//! To make it easy to construct input data from your own datatypes the Reval crate implements a serde serializer for Value types. So any type that implements `serde::Serialize` can be serialized into a `reval::Value` without writing any code.
+//!
+//! ```rust, ignore
+//! use reval::{value::Value, value::ser::ValueSerializer, parse_json::parse};
+//! use serde::Serialize;
+//!
+//! let expr = parse(r#"{"Gt": [{"Ref": "age"}, {"Int": 21}]}"#).unwrap();
+//!
+//! #[derive(Serialize)]
+//! struct Data { age: u16 }
+//!
+//! let facts = Data {age: 21}.serialize(ValueSerializer).unwrap();
+//!
+//! assert_eq!(expr.evaluate(&facts).unwrap(), true.into());
+//!
+//! ```
+
 pub mod expr;
 #[cfg(feature = "nom_parser")]
 pub mod parse;
