b2c3f17d282afdf5ee84d676b5fa0b6fc8442bab
Mendelt Siebenga <mendelt@mendelt.nl>
2023-07-25 13:58:43 +0200

    implement * / + and -

--- src/parse/expr.rs
+++ src/parse/expr.rs
@@ -19,12 +19,12 @@ mod when_parsing_integer {
 
     #[test]
     fn should_parse_integer() {
-        assert_eq!(Expr::parse("15").unwrap(), Expr::value(15));
+        assert_eq!(Expr::parse("i15").unwrap(), Expr::value(15));
     }
 
     #[test]
     fn should_parse_negative_integer() {
-        assert_eq!(Expr::parse("-6").unwrap(), Expr::value(-6))
+        assert_eq!(Expr::parse("i-6").unwrap(), Expr::value(-6))
     }
 }
 
@@ -166,56 +166,87 @@ mod when_parsing_expressions {
     //     should_parse(expr("2==8"), Expr::eq(Expr::value(2), Expr::value(8)));
     // }
 
-    // #[test]
-    // fn should_parse_subtraction() {
-    //     should_parse(expr("12-4"), Expr::sub(Expr::value(12), Expr::value(4)));
-    // }
+    #[test]
+    fn should_parse_add() {
+        assert_eq!(
+            Expr::parse("i1+i4").unwrap(),
+            Expr::add(Expr::value(1), Expr::value(4))
+        );
+    }
 
-    // #[test]
-    // fn should_parse_addition() {
-    //     should_parse(expr("1+4"), Expr::add(Expr::value(1), Expr::value(4)));
-    // }
+    #[test]
+    fn should_parse_subtraction() {
+        assert_eq!(
+            Expr::parse("i12-i4").unwrap(),
+            Expr::sub(Expr::value(12), Expr::value(4))
+        );
+    }
 
-    // #[test]
-    // fn should_parse_division() {
-    //     should_parse(expr("1/15"), Expr::div(Expr::value(1), Expr::value(15)));
-    // }
+    #[test]
+    fn should_parse_add_and_sub_left_associatively() {
+        assert_eq!(
+            Expr::parse("i1-i15+i25").unwrap(),
+            Expr::add(Expr::sub(Expr::value(1), Expr::value(15)), Expr::value(25))
+        );
+    }
 
-    // #[test]
-    // fn should_parse_multiplication() {
-    //     should_parse(expr("1*4"), Expr::mult(Expr::value(1), Expr::value(4)));
-    // }
+    #[test]
+    fn should_parse_mult() {
+        assert_eq!(
+            Expr::parse("i1*i4").unwrap(),
+            Expr::mult(Expr::value(1), Expr::value(4))
+        );
+    }
 
-    // #[test]
-    // fn should_parse_expression_inside_parentheses() {
-    //     should_parse(expr("(1+1)"), Expr::add(Expr::value(1), Expr::value(1)));
-    // }
+    #[test]
+    fn should_parse_div() {
+        assert_eq!(
+            Expr::parse("i1/i15").unwrap(),
+            Expr::div(Expr::value(1), Expr::value(15))
+        );
+    }
 
-    // #[test]
-    // fn should_parse_correct_precedence() {
-    //     should_parse(
-    //         expr("14*128+4/5*3-1"),
-    //         Expr::sub(
-    //             Expr::add(
-    //                 Expr::mult(Expr::value(14), Expr::value(128)),
-    //                 Expr::div(Expr::value(4), Expr::mult(Expr::value(5), Expr::value(3))),
-    //             ),
-    //             Expr::value(1),
-    //         ),
-    //     );
-    // }
+    #[test]
+    fn should_parse_div_and_mult_left_associatively() {
+        assert_eq!(
+            Expr::parse("i1/i15*i25").unwrap(),
+            Expr::mult(Expr::div(Expr::value(1), Expr::value(15)), Expr::value(25))
+        );
+    }
 
-    // #[test]
-    // fn should_ignore_space_and_newlines() {
-    //     should_parse(
-    //         expr("3 * 4\r\n    + 8\r\n  "),
-    //         Expr::add(Expr::mult(Expr::value(3), Expr::value(4)), Expr::value(8)),
-    //     );
-    // }
+    #[test]
+    fn should_parse_correct_precedence() {
+        assert_eq!(
+            Expr::parse("i14*i128+i4/i5*i3-i1").unwrap(),
+            Expr::sub(
+                Expr::add(
+                    Expr::mult(Expr::value(14), Expr::value(128)),
+                    Expr::mult(Expr::div(Expr::value(4), Expr::value(5)), Expr::value(3)),
+                ),
+                Expr::value(1),
+            ),
+        );
+    }
+
+    #[test]
+    fn should_ignore_space_and_newlines() {
+        assert_eq!(
+            Expr::parse("i3 * i4\r\n    +i8\r\n  ").unwrap(),
+            Expr::add(Expr::mult(Expr::value(3), Expr::value(4)), Expr::value(8)),
+        );
+    }
 
     #[test]
     fn should_parse_simple_parentheses() {
-        assert_eq!(Expr::parse("(5)").unwrap(), Expr::value(5));
+        assert_eq!(Expr::parse("(i5)").unwrap(), Expr::value(5));
+    }
+
+    #[test]
+    fn should_parse_expression_inside_parentheses() {
+        assert_eq!(
+            Expr::parse("(i1+i1)").unwrap(),
+            Expr::add(Expr::value(1), Expr::value(1))
+        );
     }
 
     // #[test]
@@ -233,14 +264,5 @@ mod when_parsing_expressions {
     // #[test]
     // fn should_parse_nested_parentheses() {
     //     todo!()
-    // }
-
-    // /// Helper function to test parsing, checks if the Result of a parse-
-    // /// operation is not an error, if there is no rest and if the parsed
-    // /// expression equals the expected value.
-    // fn should_parse(result: IResult<&str, Expr>, expected_expr: Expr) {
-    //     let (rest, parsed) = result.unwrap();
-    //     assert_eq!(parsed, expected_expr);
-    //     assert_eq!(rest, "");
     // }
 }
