97e5318b4c2b03a804df216f35c489d141732257
Mendelt Siebenga <mendelt@mendelt.nl>
2023-11-04 23:45:58 +0100

    parse symbol expressions

--- src/expr/eval.rs
+++ src/expr/eval.rs
@@ -12,6 +12,7 @@ impl Expr {
         match self {
             Expr::Value(value) => Ok(value.clone()),
             Expr::Reference(name) => reference(facts, name),
+            Expr::Symbol(name) => symbol(name, context),
             Expr::Index(value, idx) => index(value.evaluate(context, facts).await?, idx),
             Expr::Function(name, value) => {
                 let param = value.evaluate(context, facts).await?;
@@ -88,6 +89,10 @@ fn reference(facts: &Value, name: &str) -> Result<Value> {
         _ => Err(Error::InvalidType),
     }
     .map(Clone::clone)
+}
+
+fn symbol(_name: &str, _context: &EvaluationContext<'_>) -> Result<Value> {
+    Ok(Value::None) // TODO: implement
 }
 
 fn index(value: Value, index: &Index) -> Result<Value> {
