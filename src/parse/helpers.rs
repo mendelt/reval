6e699ad408d1dc258b5bfad05b7487430848a4a2
Mendelt Siebenga <mendelt@mendelt.nl>
2024-03-20 12:55:57 +0100

    add error type for parse helpers

--- src/parse/helpers.rs
+++ src/parse/helpers.rs
@@ -7,34 +7,50 @@ use crate::{
 use rust_decimal::{self, Decimal};
 use std::num::{ParseFloatError, ParseIntError};
 use std::str::FromStr;
+use thiserror;
 
-pub(crate) fn parse_int_value(value: &str) -> Result<Value, ParseIntError> {
+pub(crate) fn parse_int_value(value: &str) -> Result<Value, RevalParseError> {
     Ok(Value::Int(i128::from_str(&value[1..])?))
 }
 
-pub(crate) fn parse_hex_int_value(value: &str) -> Result<Value, ParseIntError> {
+pub(crate) fn parse_hex_int_value(value: &str) -> Result<Value, RevalParseError> {
     Ok(Value::Int(i128::from_str_radix(&value[2..], 16)?))
 }
 
-pub(crate) fn parse_bin_int_value(value: &str) -> Result<Value, ParseIntError> {
+pub(crate) fn parse_bin_int_value(value: &str) -> Result<Value, RevalParseError> {
     Ok(Value::Int(i128::from_str_radix(&value[2..], 2)?))
 }
 
-pub(crate) fn parse_oct_int_value(value: &str) -> Result<Value, ParseIntError> {
+pub(crate) fn parse_oct_int_value(value: &str) -> Result<Value, RevalParseError> {
     Ok(Value::Int(i128::from_str_radix(&value[2..], 8)?))
 }
 
-pub(crate) fn parse_float_value(value: &str) -> Result<Value, ParseFloatError> {
+pub(crate) fn parse_float_value(value: &str) -> Result<Value, RevalParseError> {
     Ok(Value::Float(f64::from_str(&value[1..])?))
 }
 
-pub(crate) fn parse_decimal_value(value: &str) -> Result<Value, rust_decimal::Error> {
+pub(crate) fn parse_decimal_value(value: &str) -> Result<Value, RevalParseError> {
     Ok(Value::Decimal(Decimal::from_str(&value[1..])?))
 }
 
-pub(crate) fn parse_string_literal(value: &str) -> Result<Value, UnescapeError> {
+pub(crate) fn parse_string_literal(value: &str) -> Result<Value, RevalParseError> {
     let unquoted = &value[1..value.len() - 1];
     let unescaped = unescape(unquoted)?;
 
     Ok(Value::String(unescaped))
+}
+
+#[derive(Clone, Debug, thiserror::Error, PartialEq)]
+pub(crate) enum RevalParseError {
+    #[error("Error parsing integer")]
+    IntParseError(#[from] ParseIntError),
+
+    #[error("Error parsing string")]
+    StringParseError(#[from] UnescapeError),
+
+    #[error("Error parsing float")]
+    FloatParseError(#[from] ParseFloatError),
+
+    #[error("Error parsing decimal")]
+    DecimalParseError(#[from] rust_decimal::Error),
 }
