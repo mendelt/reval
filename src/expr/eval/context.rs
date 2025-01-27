6b9d9b857ca336cef4d82b06631b40a4cdcefb8d
Mendelt Siebenga <mendelt@mendelt.nl>
2025-01-27 23:59:42 +0100

    clean up expression evaluation by combining params in contexg

---
+++ src/expr/eval/context.rs
@@ -0,0 +1,49 @@
+use super::FunctionCache;
+use crate::{
+    error::{Error, Result},
+    ruleset::RuleSet,
+    value::Value,
+};
+
+pub(super) struct EvalContext<'a> {
+    ruleset: &'a RuleSet,
+    function_cache: &'a mut FunctionCache,
+    facts: &'a Value,
+}
+
+impl<'a> EvalContext<'a> {
+    pub(super) fn new(
+        ruleset: &'a RuleSet,
+        function_cache: &'a mut FunctionCache,
+        facts: &'a Value,
+    ) -> Self {
+        Self {
+            ruleset,
+            function_cache,
+            facts,
+        }
+    }
+}
+
+impl EvalContext<'_> {
+    pub(super) async fn call_function(&mut self, name: &str, params: Value) -> Result<Value> {
+        self.ruleset
+            .call_function(name, params, self.function_cache)
+            .await
+    }
+
+    pub(super) fn reference(&self, name: &str) -> Result<Value> {
+        match self.facts {
+            value if name == "facts" => Ok(value),
+            Value::Map(facts) => facts
+                .get(name)
+                .ok_or_else(|| Error::UnknownRef(name.to_owned())),
+            _ => Err(Error::InvalidType),
+        }
+        .cloned()
+    }
+
+    pub(super) async fn symbol(&mut self, name: &str) -> Result<Value> {
+        self.ruleset.get_symbol(name)?.eval_rec(self).await
+    }
+}
